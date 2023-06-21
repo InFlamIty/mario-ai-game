ec9e16ac3bc52c4ac89f761c40d901e9198b0e4a
InFlamIty <107593393+InFlamIty@users.noreply.github.com>
2023-06-21 19:24:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav")
+	mario_kill = loadSound("kick.wav")
+	mario_die = loadSound("mariodie.wav")
+	mario_coin = loadSound("coin.wav")
+	gameover = loadSound("gameover.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas")
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+    video.size(800,400);
+	video.parent("game_console")
+	poseNet = ml5.poseNet(video , modelloaded)
+	poseNet.on("pose" , gotPoses)
+}
+
+function draw() {
+	game()
+}
+
+function modelloaded()
+{
+	console.log("it worked after the incident, so hopefully it works this ti ............................... NOT AGAIN, AAAAAAAAAHHHHHHHHHHHHH!")
+}
+
+function gotPoses(results)
+{
+	if (results.length > 0) {
+		console.log(results)
+		noseX = results[0].pose.nose.x
+		noseY = results[0].pose.nose.y
+	}
+}
+
+
+
+
+
+
